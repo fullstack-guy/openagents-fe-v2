ede002458b4d9514a5bfd8696db289476c1cf9b6
othmanezoheir <zozoheir@umich.edu>
2023-06-08 14:12:11 -0400

    successful v2 rebuild light

--- src/App.js
+++ src/App.js
@@ -1,28 +1,19 @@
 import {CssBaseline, ThemeProvider} from '@mui/material';
 import {useRoutes} from 'react-router-dom';
-import {useSelector} from 'react-redux';
 import {ThemeSettings} from './theme/Theme';
-import RTL from './layouts/full/shared/customizer/RTL';
 import ScrollToTop from './components/shared/ScrollToTop';
 import Router from './routes/Router';
 import "./theme/styles.css";
-import "./App.css"
 
 function App() {
     const routing = useRoutes(Router);
     const theme = ThemeSettings();
-    const customizer = useSelector((state) => state.customizer);
 
     return (
 
         <ThemeProvider theme={theme}>
-            <script src="https://www.gstatic.com/firebasejs/8.10.0/firebase-app.js"></script>
-            <script src="https://www.gstatic.com/firebasejs/8.10.0/firebase-auth.js"></script>
-
-            <RTL direction={customizer.activeDir}>
-                <CssBaseline/>
-                <ScrollToTop>{routing}</ScrollToTop>
-            </RTL>
+            <CssBaseline/>
+            <ScrollToTop>{routing}</ScrollToTop>
         </ThemeProvider>
     );
 }
