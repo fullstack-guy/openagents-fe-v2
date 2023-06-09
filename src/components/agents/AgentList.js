f23bbb815d8a44fc1f3cc134c2466fc85052bfa0
othmanezoheir <zozoheir@umich.edu>
2023-06-09 18:47:01 -0400

    working create agent formik

--- src/components/agents/AgentList.js
+++ src/components/agents/AgentList.js
@@ -1,99 +1,103 @@
-import React, { useEffect } from 'react';
-import { List } from '@mui/material';
-import { useSelector, useDispatch } from 'react-redux';
+import React, {useEffect} from 'react';
+import {List} from '@mui/material';
+import {useSelector, useDispatch} from 'react-redux';
 import {
-  selectAgent,
-  fetchAgents,
-  deleteAgent,
+    selectAgent,
+    fetchAgents,
+    deleteAgent,
 } from 'src/store/AgentSlice';
 
 import Scrollbar from 'src/components/custom-scroll/Scrollbar';
 import AgentListItem from './AgentListItem';
+import AgentAdd from './AgentAdd';
 
-const AgentList = ({ showrightSidebar }) => {
-  const dispatch = useDispatch();
-  useEffect(() => {
-    dispatch(fetchAgents());
-  }, [dispatch]);
+const AgentList = ({showrightSidebar}) => {
+    const dispatch = useDispatch();
+    useEffect(() => {
+        dispatch(fetchAgents());
+    }, [dispatch]);
 
-  const getVisibleContacts = (agents, filter, agentSearch) => {
-    switch (filter) {
-      case 'show_all':
-        return agents.filter(
-          (a) => !a.deleted && a.agent_configs.name.toLocaleLowerCase().includes(agentSearch),
-        );
+    const getVisibleContacts = (agents, filter, agentSearch) => {
+        switch (filter) {
+            case 'show_all':
+                return agents.filter(
+                    (a) => !a.deleted && a.agent_configs.name.toLocaleLowerCase().includes(agentSearch),
+                );
 
-      case 'frequent_agent':
-        return agents.filter(
-          (a) =>
-            !a.deleted &&
-            a.frequentlyagented &&
-            a.agent_configs.name.toLocaleLowerCase().includes(agentSearch),
-        );
+            case 'frequent_agent':
+                return agents.filter(
+                    (a) =>
+                        !a.deleted &&
+                        a.frequentlyagented &&
+                        a.agent_configs.name.toLocaleLowerCase().includes(agentSearch),
+                );
 
-      case 'starred_agent':
-        return agents.filter(
-          (a) => !a.deleted && a.starred && a.agent_configs.name.toLocaleLowerCase().includes(agentSearch),
-        );
+            case 'starred_agent':
+                return agents.filter(
+                    (a) => !a.deleted && a.starred && a.agent_configs.name.toLocaleLowerCase().includes(agentSearch),
+                );
 
-      case 'engineering_department':
-        return agents.filter(
-          (a) =>
-            !a.deleted &&
-            a.department === 'Engineering' &&
-            a.agent_configs.name.toLocaleLowerCase().includes(agentSearch),
-        );
+            case 'engineering_department':
+                return agents.filter(
+                    (a) =>
+                        !a.deleted &&
+                        a.department === 'Engineering' &&
+                        a.agent_configs.name.toLocaleLowerCase().includes(agentSearch),
+                );
 
-      case 'support_department':
-        return agents.filter(
-          (a) =>
-            !a.deleted &&
-            a.department === 'Support' &&
-            a.agent_configs.name.toLocaleLowerCase().includes(agentSearch),
-        );
+            case 'support_department':
+                return agents.filter(
+                    (a) =>
+                        !a.deleted &&
+                        a.department === 'Support' &&
+                        a.agent_configs.name.toLocaleLowerCase().includes(agentSearch),
+                );
 
-      case 'sales_department':
-        return agents.filter(
-          (a) =>
-            !a.deleted &&
-            a.department === 'Sales' &&
-            a.agent_configs.name.toLocaleLowerCase().includes(agentSearch),
-        );
+            case 'sales_department':
+                return agents.filter(
+                    (a) =>
+                        !a.deleted &&
+                        a.department === 'Sales' &&
+                        a.agent_configs.name.toLocaleLowerCase().includes(agentSearch),
+                );
 
-      default:
-        throw new Error(`Unknown filter: ${filter}`);
-    }
-  };
+            default:
+                throw new Error(`Unknown filter: ${filter}`);
+        }
+    };
 
 
-  const agents = useSelector((state) =>
-    getVisibleContacts(
-      state.agentsReducer.agents,
-      state.agentsReducer.currentAgentFilter,
-      state.agentsReducer.agentSearch,
-    ),
-  );
+    const agents = useSelector((state) =>
+        getVisibleContacts(
+            state.agentsReducer.agents,
+            state.agentsReducer.currentAgentFilter,
+            state.agentsReducer.agentSearch,
+        ),
+    );
 
-  const active = useSelector((state) => state.agentsReducer.agentData);
+    const active = useSelector((state) => state.agentsReducer.agentData);
 
-  return (
-    <List>
-      <Scrollbar sx={{ height: { lg: 'calc(100vh - 100px)', md: '100vh' }, maxHeight: '800px' }}>
-        {agents.map((agent) => (
-          <AgentListItem
-            key={agent.id}
-            active={agent.id === active}
-            {...agent}
-            onContactClick={() => {
-              dispatch(selectAgent(agent.id));
-              showrightSidebar();
-            }}
-            onDeleteClick={() => dispatch(deleteAgent(agent.id))}
-          />
-        ))}
-      </Scrollbar>
-    </List>
-  );
+    return (
+        <>
+            <AgentAdd/>
+            <List>
+                <Scrollbar sx={{height: {lg: 'calc(100vh - 100px)', md: '100vh'}, maxHeight: '800px'}}>
+                    {agents.map((agent) => (
+                        <AgentListItem
+                            key={agent.id}
+                            active={agent.id === active}
+                            {...agent}
+                            onContactClick={() => {
+                                dispatch(selectAgent(agent.id));
+                                showrightSidebar();
+                            }}
+                            onDeleteClick={() => dispatch(deleteAgent(agent.id))}
+                        />
+                    ))}
+                </Scrollbar>
+            </List>
+        </>
+    );
 };
 
 export default AgentList;
